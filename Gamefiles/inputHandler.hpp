aa6b84c048673f7c6613e988821418bf7a42d206
Xiao yi Hu <xy1998@live.nl>
2020-01-14 16:44:29 +0100

    added interactCommand.hpp, and added interaction functionality to inputHandler

--- Gamefiles/inputHandler.hpp
+++ Gamefiles/inputHandler.hpp
@@ -4,8 +4,9 @@
 #include <SFML/Graphics.hpp>
 #include <array>
 #include "command.hpp"
+#include "interactCommand.hpp"
 #include "moveCommand.hpp"
-#include "gameStorage.hpp"
+#include "objectStorage.hpp"
 
 class inputHandler{
 private:
@@ -13,20 +14,41 @@ private:
         sf::Keyboard::Up, sf::Keyboard::Down,
         sf::Keyboard::Left, sf::Keyboard::Right
     };
-    gameStorage &inputStorage;
+
+    std::array<sf::Keyboard::Key, 2> interactionKeys ={
+        sf::Keyboard::E,
+        sf::Keyboard::Up
+    };
+
+    objectStorage &inputStorage;
     
 public:
 
-    inputHandler(gameStorage &inputStorage):
+    inputHandler(objectStorage &inputStorage):
     inputStorage(inputStorage){}
 
     command* handleInput(){
 
-        for( auto i : moveKeys){
-            if(sf::Keyboard::isKeyPressed(i)){
+        for( auto movementKey : moveKeys){
+            if(sf::Keyboard::isKeyPressed(movementKey)){
                 auto j = inputStorage.get()[0];
-                command* newMoveCommand = new moveCommand( i, inputStorage.getObject() );
-                return newMoveCommand;
+                return new moveCommand( movementKey, inputStorage.getObject() );
+            }
+        }
+
+        for( auto interactKey : interactionKeys){
+            if(sf::Keyboard::isKeyPressed(interactKey)){
+
+                for(std::shared_ptr<gameObject> objectPointer : inputStorage.game){
+
+                    std::shared_ptr<gameObject> closestInteractablePointer = NULL; //needs to be changed to a "nullObject" ie distance = infinite
+                    if(objectPointer->interactable && objectPointer->inRange() && objectPointer->CurrentDistance() < closestInteractablePointer->currentDistance()){
+                        closestInteractable = objectPointer;
+                    }
+
+                    return new interactCommand(closestInteractablePointer);
+                }
+
             }
         }
 
---
+++ Gamefiles/interactCommand.hpp
@@ -0,0 +1,24 @@
+#ifndef _INTERACT_COMMAND_HPP
+#define _INTERACT_COMMAND_HPP
+
+#include "command.hpp"
+#include <iostream>
+#include <map>
+#include "memory"
+
+class interactCommand : public command{
+private:
+    std::shared_ptr<gameObject> toInteractObject;
+
+public:
+    interactCommand(std::shared_ptr<gameObject> toInteractObject):
+        toInteractObject(toInteractObject)
+    {}
+
+    void execute() override{
+        toInteractObject.get()->interact();
+    }
+
+    virtual void undo(){}
+};
+#endif
