ec519326db9c016492895d2166123587fcd05fe7
Xiao yi Hu <xy1998@live.nl>
2020-01-20 14:34:09 +0100

    added more functions for accessing lanes through board, added system for effects in lane class

--- Gamefiles/board.hpp
+++ Gamefiles/board.hpp
@@ -3,6 +3,7 @@
 
 #include "memory"
 #include "gameObject.hpp"
+#include "card.hpp"
 
 #define LANE_SIZE 7
 
@@ -13,6 +14,7 @@ enum E_lane{
 class lane{
 private:
     std::shared_ptr<gameObject> laneArray[LANE_SIZE];
+    std::vector<std::shared_ptr<gameObject>> laneEffects[LANE_SIZE];
 public:
     lane():
         laneArray{nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}
@@ -30,14 +32,33 @@ public:
         laneArray[index] = unitPointer;
     }
 
+    void placeEffect(const int index, std::shared_ptr<gameObject> effectPointer){
+        laneArray[index].push_back(effectPointer);
+    }
+
     void updateLane(){
         for(uint_fast8_t i = 0; i < LANE_SIZE; i++){
             laneArray[i]->update(i, this);
+        }
+
+        //for every position
+        for(uint_fast8_t i = 0; i < LANE_SIZE; i++){
+            //for every spell on that position
+            for(uint_fast8_t j = 0; j < laneEffects[i]; j++){
+                laneEffects[i][j]->update(i, j, this);
+            }
         }
     }
 
     void removeAtIndex(const int index){
         laneArray[index] = nullptr;
+    }
+
+    void removeEffectAtIndex(const int positionIndex, const int effectIndex){
+        auto toRemoveEffect = laneEffects[positionIndex][effectIndex];
+        laneEffects[positionIndex][effectIndex] = laneEffects[positionIndex].back();
+        laneEffects[positionIndex][laneEffects[positionIndex].size() - 1] = toRemoveEffect;
+        laneEffects[positionIndex].pop_back();
     }
 };
 
@@ -63,19 +84,38 @@ public:
     void placeUnit(const int E_lane, const int laneIndex, std::shared_ptr<gameObject> unitPointer){
         lanes[E_lane].placeUnit(laneIndex, unitPointer);
     }
-
-    void castSpell(const int E_lane, const int index, std::shared_ptr<gameObject> spell){
-        spell.activate(lanes, E_lane, index);
+    
+    void placeTrapcard(const int index, std::shared_ptr<gameObject> trapcardPointer){
+        lanes[E_lane::trapLane].placeUnit(index, trapcardPointer);
     }
 
-    void placeSpell(const int index, std::shared_ptr<gameObject> trapcardPointer){
-        lanes[E_lane::trapLane].placeUnit(index, trapcardPointer);
+    void placeEffect(const int E_lane, const int index, std::shared_ptr<gameObject> effectPointer){
+        lanes[E_lane].placeEffect(index, effectPointer)
+    }
+
+    void castSpell(const int E_lane, const int index, std::shared_ptr<gameObject> spell){
+        spell.activate(&lanes, E_lane, index);
+    }
+
+    void removeUnit(const int E_lane, const int index){
+        lanes[E_lane].removeAtIndex(index);
+    }
+
+    void removeEffect(const int E_lane, const int index, const int effectIndex){
+        lanes[E_lane].removeEffectAtIndex(index, effectIndex);
     }
 
     std::shared_ptr<gameObject> getUnitPointer(const int E_lane, const int laneIndex){
         return lanes[E_lane].getUnitPointerAtIndex(laneIndex);
     }
 
+    bool isPositionEmpty(const int E_lane, const int index){
+        return lanes[E_lane].isIndexEmpty(index);
+    }
+
+    bool isTrapCardOnPosition(const int index){
+        return lanes[E_lane:::trapLane].isIndexEmpty(index);
+    }
 };
 
 #endif
