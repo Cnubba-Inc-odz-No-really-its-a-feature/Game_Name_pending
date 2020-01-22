c8db3bc09234bde5afdf04fb979ba385b269e148
pieter_ruijs <pieter.ruijs@student.hu.nl>
2020-01-22 15:54:05 +0100

    chest animiation

--- Gamefiles/chest.hpp
+++ Gamefiles/chest.hpp
@@ -11,18 +11,21 @@ class chest: public gameObject{
 private:
     std::vector<gameObject> lootObjectVector;
     bool open = false;
+    int textureFrame = 0;
+    int frameCounter = 0;
+    bool interacted = false;
 public:
     chest(sf::Vector2f spritePosition, sf::Vector2f spriteScale, std::map<std::string, sf::Texture> textureMap, std::string firstKey, int objectPriority):
         gameObject(spritePosition, spriteScale, textureMap, firstKey)
     {
         interactable = true;
         gameObject::objectPriority = objectPriority;
-	    objectSprite.setTextureRect(sf::IntRect(0, 0, 20, 16));
+	    objectSprite.setTextureRect(sf::IntRect(0, 0, 16, 30));
     }
 
     void interact() override{
-           objectSprite.setTextureRect(sf::IntRect(20, 0, 20, 16));
-     
+        std::cout << textureFrame << std::endl;
+        interacted = true;
     }
 
     void interact(objectStorage& gameStorage, const float& mainCharacterPosition){
@@ -30,6 +33,7 @@ public:
     }
 
     void draw(sf::RenderWindow& gameWindow) override{
+        if(interacted) setFrame(3, 0);
         gameWindow.draw(objectSprite);
         if(open){
             for(auto& loot : lootObjectVector){
@@ -42,7 +46,11 @@ public:
 
     void update(){}
 
-    void setFrame(int maxFrame, int textureRow){
+    void setFrame(int maxFrame, int textureRow) override{
+        if(frameCounter > 10) {frameCounter = 0; textureFrame++;}
+	    objectSprite.setTextureRect(sf::IntRect(16*textureFrame, 0*textureRow, 16, 30));
+        if(maxFrame <= textureFrame) interacted = false;
+	    else frameCounter++;
     }
 
 };
