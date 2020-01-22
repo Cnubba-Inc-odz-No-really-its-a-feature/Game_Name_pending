902829d8cb90145236743ab7c53ab700dfc86c09
Xiao yi Hu <xy1998@live.nl>
2020-01-22 16:22:06 +0100

    created hpp file for fight controlelr

---
+++ Gamefiles/fightController.hpp
@@ -0,0 +1,15 @@
+#ifndef _FIGHT_CONTROLLER_HPP
+#define _FIGHT_CONTROLLER_HPP
+
+#include "deck.hpps"
+
+class fightController{
+private:
+    deck deck
+public:
+    fightController(){
+
+    }
+};
+
+#endif
