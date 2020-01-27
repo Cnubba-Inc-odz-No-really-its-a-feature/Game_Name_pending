c179cc783c95ed65805b16beed231ab17875b6db
v1oopc-ubuntu <ties1000@live.nlgit config -- glocal user.name Matthies>
2020-01-27 09:09:40 -0800

    added new cards

--- Gamefiles/card.hpp
+++ Gamefiles/card.hpp
@@ -337,7 +337,8 @@ public:
         discardPile(discardPile),
         completeDeck(completeDeck),
         cardsInHand(cardsInHand),
-        cardHand(discardPile){
+        cardHand(discardPile),
+        deckViewer(completeDeck){
 
             completeDeck = {1,1,1,2,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10};
 
